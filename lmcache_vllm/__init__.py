9ca3b0d1df9bdb1f82bbec7649d83735aacb8be3
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-09-15 07:11:33 -0500

    add temporary comment

--- lmcache_vllm/__init__.py
+++ lmcache_vllm/__init__.py
@@ -1,8 +1,8 @@
 from lmcache_vllm.driver_v2 import LMCVLLMDriver_V2
-from lmcache_vllm.utils import broadcast_tokens_and_block_tables, broadcast_input_ids_list
+#from lmcache_vllm.utils import broadcast_tokens_and_block_tables, broadcast_input_ids_list
 
 __all__ = [
     "LMCVLLMDriver_V2",
-    "broadcast_tokens_and_block_tables",
-    "broadcast_input_ids_list",
+    #"broadcast_tokens_and_block_tables",
+    #"broadcast_input_ids_list",
 ]
