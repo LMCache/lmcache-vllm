978fcb1b0e7c2356cf641dc2335317abfb35bb31
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-09-17 10:06:58 -0500

    bugfix and add multiprocessing

--- examples/entrypoints.py
+++ examples/entrypoints.py
@@ -5,23 +5,26 @@ from lmcache.cache_engine import LMCacheEngineBuilder
 from lmcache_vllm import LMCVLLMDriver_V2#, broadcast_tokens_and_block_tables, broadcast_input_ids_list
 from lmcache.config import LMCacheEngineConfig, LMCacheEngineMetadata
 
-if __name__ == '__main__':
+def init_lmc(
+    vllm_config,
+    lmcache_config_file,
+    vllm_rank: int,
+    vllm_world_size: int):
     
-    # TODO(Jiayi): Most configs are hard-coded in yaml for now
-    # Maybe they can be sent from vllm during init
-    vllm_config = yaml.safe_load(open("vllm_config.yaml"))
-    
-    lmcache_config_file = "lmcache_config.yaml"
+    # create vllm-lmc pairs
+    for i in range(vllm_world_size):
+        # vllm rank: i
+        # lmc rank: i + world_size
+        group_ranks.append([i, i + vllm_world_size])
     
     # TODO(Jiayi): need to use multiprocessing to launch multiple cache engines
-    vllm_rank = 0
-    vllm_world_rank = 1
-    lmc_rank = 1
+    lmc_rank = vllm_rank + vllm_world_size
     
-    
+    # FIXME(Jiayi): using `vllm_world_size` is incorrect
+    # e.g., tp(2)*pp(4) = tp(1)*pp(8)
     lmcache_metadata = LMCacheEngineMetadata(
-        "mistral-7b",
-        vllm_world_rank,
+        vllm_config.get('model_name'),
+        vllm_world_size,
         vllm_rank,
         fmt="vllm")
     lmcache_config = LMCacheEngineConfig.from_file(lmcache_config_file)
@@ -33,4 +36,21 @@ if __name__ == '__main__':
     
     # Start LMC
     lmcache_driver.run()
+
+
+if __name__ == '__main__':
+    
+    # TODO(Jiayi): Most configs are hard-coded in yaml for now
+    # Maybe they can be sent from vllm during init
+    vllm_config = yaml.safe_load(open("vllm_config.yaml"))
+    lmcache_config_file = "lmcache_config.yaml"
+    
+    tp = vllm_config.get("tensor_model_parallel_size")
+    pp = vllm_config.get("pipeline_model_parallel_size")
+    vllm_world_size = tp * pp
+    
+    for vllm_rank in range(vllm_world_size):
+        p = Process(target=init_lmc, args=(vllm_config, lmcache_config_file, vllm_rank, vllm_world_size))
+        p.start()
+    p.join()
     
