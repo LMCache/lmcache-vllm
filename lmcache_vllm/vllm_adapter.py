9e35ec29e620f258f2728971b1e477b0c8e8ba0f
YaoJiayi <120040070@link.cuhk.edu.cn>
2024-11-08 03:34:11 -0600

    fix format

--- lmcache_vllm/vllm_adapter.py
+++ lmcache_vllm/vllm_adapter.py
@@ -30,19 +30,18 @@ logger = init_logger(__name__)
 ENGINE_NAME = "vllm-instance"
 LMCACHE_CUDA_STREAM = torch.cuda.Stream()
 
-TORCH_DTYPE_TO_STR_DTYPE = {
-    torch.half: "half",
-    torch.float16: "half",
-    torch.bfloat16: "bfloat16",
-    torch.float: "float",
-    torch.float32: "float",
-    torch.float64: "double",
-    torch.double: "double",
-    torch.uint8: "fp8",
-    torch.float8_e4m3fn: "fp8_e4m3", 
-    torch.float8_e5m2: "fp8_e5m2",
+# FIXME(Jiayi): Not sure if this is needed
+STR_DTYPE_TO_TORCH_DTYPE = {
+    "half": torch.half,
+    "bfloat16": torch.bfloat16,
+    "float": torch.float,
+    "float32": torch.float32,
+    "float64": torch.float64,
+    "double": torch.double,
+    "fp8": torch.uint8,
+    "fp8_e4m3": torch.float8_e4m3fn,
+    "fp8_e5m2": torch.float8_e5m2,
 }
-
 class StoreStatus(Enum):
     PREFILL = 1
     CHUNK_PREFILL = 2
@@ -164,21 +163,23 @@ def init_lmcache_engine(
         return 
 
     config = lmcache_get_config()
+    
+    kv_dtype = get_kv_cache_torch_dtype(cache_config.cache_dtype, model_config.dtype)
 
-    # If KV cache's dtype is "auto", enforce it to be the same with model's dtype
-    if cache_config.cache_dtype == "auto":
-        kv_cache_torch_dtype = get_kv_cache_torch_dtype(cache_config.cache_dtype,
-                                                        model_config.dtype)
-        kv_cache_str_dtype = TORCH_DTYPE_TO_STR_DTYPE[kv_cache_torch_dtype]
-    else:
-        kv_cache_str_dtype = cache_config.cache_dtype
-
+    # construct kv shape (for mem pool)
+    num_layer = model_config.get_num_layers(parallel_config)
+    chunk_size = config.chunk_size
+    num_kv_head = model_config.get_num_kv_heads(parallel_config)
+    head_size = model_config.get_head_size()
+    kv_shape = (num_layer, 2, chunk_size, num_kv_head, head_size)
+    
     metadata = LMCacheEngineMetadata(
             model_config.model,
             parallel_config.world_size,
             parallel_config.rank,
             "vllm",
-            kv_cache_str_dtype)
+            kv_dtype,
+            kv_shape)
     
     engine = LMCacheEngineBuilder.get_or_create(
             ENGINE_NAME,
